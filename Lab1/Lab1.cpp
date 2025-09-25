a9d59d7ca71f0e3ffe21598b0b936e7c63414a26
nykqqq <nayokopm@gmail.com>
2025-09-25 10:26:23 +0200

    Add files via upload

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <limits> 
+
+int main() {
+	setlocale(LC_ALL, "RU"); 
+	int unsigned a, b, h;
+	std::cout << "Значение высоты h:";
+	std::cin >> h;
+	if (h > 255) {
+		std::cout << "Ошибка, слишком большое значение";
+	}
+	else {
+		std::cout << "Значение нижнего основания a:";
+		std::cin >> a;
+		std::cout << "Значение верхнего основания b:";
+		std::cin >> b;
+	}
+	if (a > 1000 or b > 1000) {
+		std::cout << "Ошибка, слишком большое значение";
+	}
+	else {
+		int s = ((a + b) / 2) * h;
+		std::cout << "Площадь трапеции:" << s << std::endl;
+		std::cout << "int,занимает " << 128 << " бит\n" << "min: " << std::numeric_limits<int>::min() << "\n"
+			<< "max: " << std::numeric_limits<int>::max() << "\n";
+	}
+}
+   
